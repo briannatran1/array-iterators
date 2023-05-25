738faaafac19ced4e7ade348bdeabd66dd6c4859
Brianna Tran <112038594+maserabri@users.noreply.github.com>
2023-05-24 19:58:25 -0700

    Create goodIdeas.js

---
+++ goodIdeas.js
@@ -0,0 +1,21 @@
+/*For every good kata idea there seem to be quite a few bad ones!
+
+In this kata you need to check the provided array (x) for good ideas 'good' and bad ideas 'bad'. If there are one or two good ideas, return 'Publish!', if there are more than 2 return 'I smell a series!'. If there are no good ideas, as is often the case, return 'Fail!'.*/
+
+function well(arr){
+  let ideas = arr.reduce(function(acc, idea){
+    if(idea === 'good'){
+      acc++;
+    }
+    return acc;
+  }, 0);
+  if(ideas > 2){
+      return 'I smell a series!';
+  }
+  else if(ideas >= 1){
+    return'Publish!';
+  }
+  else{
+    return 'Fail!';
+  }
+}
