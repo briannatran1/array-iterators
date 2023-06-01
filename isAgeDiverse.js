8fc91674ee58786d15ec7041ca0dda0d4bfa019e
Brianna Tran <112038594+maserabri@users.noreply.github.com>
2023-06-01 13:49:49 -0700

    Update isAgeDiverse.js

--- isAgeDiverse.js
+++ isAgeDiverse.js
@@ -20,7 +20,15 @@ var list1 = [
 ];
 your function should return true as there is at least one developer from each age group.*/
 
-//declare first digit in ages in arr
+
+// declare an array ageGroup with the names of the required age groups.
+//The function uses the every() method to iterate over each age group in the ageGroup array and check if all of them pass a certain condition.
+//Inside the every() method, it uses the some() method to check if there is at least one developer in the list that matches the current age group.
+//For each age group, it compares the age of each developer
+//If there is at least one developer whose age falls within the specified range for the current age group, the some() method returns true.
+//If all the required age groups pass the check, meaning there is at least one developer from each age group, the every() method returns true.
+//If any of the required age groups fail the check, meaning at least one age group is not represented, the every() method returns false.
+//The isAgeDiverse function returns the result of the every() method, indicating whether developers from all the required age groups are represented.
 
 function isAgeDiverse(list) {
   let ageGroup = ['teens', 'twenties', 'thirties', 'forties', 'fifties', 'sixties', 'seventies', 'eighties', 'nineties', 'centenarian'];
@@ -31,7 +39,31 @@ function isAgeDiverse(list) {
         return age >= 13 && age <= 19;
       }
       else if (ageRange === 'twenties'){
-        
+        return age >= 20 && age <= 29;
+      }
+      else if(ageRange === 'thirties'){
+        return age >= 30 && age <= 39;
+      }
+      else if(ageRange === 'forties'){
+        return age >= 40 && age <= 49;
+      }
+      else if(ageRange === 'fifties'){
+        return age >= 50 && age <= 59;
+      }
+      else if(ageRange === 'sixties'){
+        return age >= 60 && age <= 69;
+      }
+      else if(ageRange === 'seventies'){
+        return age >= 70 && age <= 79;
+      }
+      else if(ageRange === 'eighties'){
+        return age >= 80 && age <= 89;
+      }
+      else if(ageRange === 'nineties'){
+        return age >= 90 && age <= 99;
+      }
+      else if(ageRange === 'centenarian'){
+        return age >= 100;
       }
     });
   });
