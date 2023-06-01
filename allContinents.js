85cec104972e87ca2e7efa5e56ed99835956f736
Brianna Tran <112038594+maserabri@users.noreply.github.com>
2023-05-31 18:01:21 -0700

    Create allContinents.js

---
+++ allContinents.js
@@ -0,0 +1,31 @@
+/*You will be given a sequence of objects (associative arrays in PHP) representing data about developers who have signed up to attend the next coding meetup that you are organising.
+
+Your task is to return:
+
+true if all of the following continents / geographic zones will be represented by at least one developer: 'Africa', 'Americas', 'Asia', 'Europe', 'Oceania'.
+false otherwise.
+For example, given the following input array:
+
+var list1 = [
+  { firstName: 'Fatima', lastName: 'A.', country: 'Algeria', continent: 'Africa', age: 25, language: 'JavaScript' },
+  { firstName: 'Agustín', lastName: 'M.', country: 'Chile', continent: 'Americas', age: 37, language: 'C' },
+  { firstName: 'Jing', lastName: 'X.', country: 'China', continent: 'Asia', age: 39, language: 'Ruby' },
+  { firstName: 'Laia', lastName: 'P.', country: 'Andorra', continent: 'Europe', age: 55, language: 'Ruby' },
+  { firstName: 'Oliver', lastName: 'Q.', country: 'Australia', continent: 'Oceania', age: 65, language: 'PHP' },
+];
+your function should return true as there is at least one developer from the required 5 geographic zones.*/
+
+//declare continents arr
+//use every to apply to the continentss arr and check each continent. will return truen if all some functions pass
+//use some on input list and checks if one dev matches that continent
+  //inside the some function, if dev is from current continent,
+    //some function will return true
+
+function allContinents(list) {
+  let continents = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+  return continents.every(function(continent){
+    return list.some(function(dev){
+      return dev.continent === continent;
+    });
+  });
+}
