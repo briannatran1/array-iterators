548f2d69db3689cf08c24c6faae59a2df24d954c
Brianna Tran <112038594+maserabri@users.noreply.github.com>
2023-06-02 15:51:58 -0700

    Create foodOption.js

---
+++ foodOption.js
@@ -0,0 +1,34 @@
+/*You will be given an array of objects representing data about developers who have signed up to attend the next coding meetup that you are organising.
+
+Your task is to return an object which includes the count of food options selected by the developers on the meetup sign-up form..
+
+For example, given the following input array:
+
+var list1 = [
+  { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'C', 
+    meal: 'vegetarian' },
+  { firstName: 'Anna', lastName: 'R.', country: 'Liechtenstein', continent: 'Europe', age: 52, language: 'JavaScript', 
+    meal: 'standard' },
+  { firstName: 'Ramona', lastName: 'R.', country: 'Paraguay', continent: 'Americas', age: 29, language: 'Ruby', 
+    meal: 'vegan' },
+  { firstName: 'George', lastName: 'B.', country: 'England', continent: 'Europe', age: 81, language: 'C', 
+    meal: 'vegetarian' },
+];
+your function should return the following object (the order of properties does not matter):
+
+{ vegetarian: 2, standard: 1, vegan: 1 }*/
+
+//use reduce to create empty object with food as keys
+
+function orderFood(list) {
+  return list.reduce(function(acc, curr){
+    let food = curr.meal;
+    if(food in acc){
+      acc[food]++;
+    }
+    else{
+      acc[food] = 1;
+    }
+    return acc;
+  }, {});
+}
