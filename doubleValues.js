bac15712810c717f5f9ead6dd0d801278eb5b9bf
Brianna Tran <112038594+briannatran1@users.noreply.github.com>
2023-07-11 23:31:31 -0700

    Create doubleValues.js

---
+++ doubleValues.js
@@ -0,0 +1,7 @@
+/*Write a function, doubleValues, which accepts an array of numbers, nums, and returns a new array containing those numbers doubled.*/
+
+//use map to create a new arr with each val doubled
+
+function doubleValues(nums) {
+    return nums.map(val => val * 2);
+}
